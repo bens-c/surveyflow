a3acc2df69aeeb91e6e1abf1035a48d85ecc8c24
Anonymous <benjamin.schroeder@sts-old.de>
2026-08-24 16:01:27 +0200

    Add API configuration

---
+++ config.js
@@ -0,0 +1,3 @@
+// Nach dem Deploy des Backends hier die URL eintragen, z. B.:
+// window.SURVEYFLOW_API_URL = "https://surveyflow-api.onrender.com";
+window.SURVEYFLOW_API_URL = window.SURVEYFLOW_API_URL || "";
